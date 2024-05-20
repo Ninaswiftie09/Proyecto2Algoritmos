48d6886e3de7ae9c6fea5d8b65a64ca1cb20f05a
Ninaswiftie09 <131787063+Ninaswiftie09@users.noreply.github.com>
2024-05-19 23:37:53 -0600

    Update Neo4jConnector.java

--- Neo4jConnector.java
+++ Neo4jConnector.java
@@ -7,9 +7,8 @@ public class Neo4jConnector {
     private final Driver driver;
 
     public Neo4jConnector() {
-        // Configura la URL de conexión con tu instancia en la nube de Neo4j
         String uri = "neo4j+s://0cc488f6.databases.neo4j.io:7687";
-        // Proporciona las credenciales de autenticación
+
         String username = "neo4j";
         String password = "fq88Qs_Hv105Tqc8dL8KH27Ir0eoyneu9gNkQFGxY3g";
         this.driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password));
